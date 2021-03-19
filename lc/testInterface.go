db385c2a2eb05d42eb0d16ef45c62fef3c8221d0
sunlong0724 <sunlong0724@sina.com>
2021-03-20 06:19:10 +0800

    update.

--- lc/testInterface.go
+++ lc/testInterface.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Cat struct{
 }
 type Duck interface {
@@ -15,3 +20,19 @@ func (c *Cat)Quack(){
 
 //var d Duck = Cat{}  用指针类型实现接口方法，结构体变量是没有实现接口的。
 //var d Duck = &Cat{}
+
+type Server int
+
+var (
+	servers  = [10]Server{1,2,3,4,5,6,7,8,9,10}
+)
+
+func Selector() Server{
+	return servers[ rand.Intn(len(servers))]
+}
+
+func main(){
+	for i := 0; i < 100; i++{
+		fmt.Println(Selector())
+	}
+}
